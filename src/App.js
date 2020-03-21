0525e38c40aebfd4672eca7a3f88a73c9af1a677
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-03-20 18:42:44 -0700

    added component folders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-
-// Normalizes string as a slug - a string that is safe to use
-// in both URLs and html attributes
 import slugify from 'slugify';
-
 import './App.css';
 
 // This object will allow us to
